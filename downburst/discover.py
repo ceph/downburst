157f9f06f92c1a8300e90959bdc6d3ef85253546
Kyr Shatskyy <kyr@clyso.com>
2026-07-21 22:47:42 +0000

    discover: lookup vault archive for alma and rocky for older versions
    Signed-off-by: Kyr Shatskyy <kyr@clyso.com>

--- downburst/discover.py
+++ downburst/discover.py
@@ -544,7 +544,11 @@ class AlmaHandler(DistroHandler):
             arch = "x86_64"
         if arch == "arm64":
             arch = "aarch64"
-        base_url = self.URL + f"/almalinux/{release}/cloud/{arch}/images"
+        (major, minor) = (int(i) for i in release.split('.'))
+        if (major == 10 and minor < 2) or (major == 9 and minor < 8) or (major == 8 and minor < 10):
+            base_url = f"https://vault.almalinux.org/{release}/cloud/{arch}/images"
+        else:
+            base_url = self.URL + f"/almalinux/{release}/cloud/{arch}/images"
         filename, serial = self.get_latest_release_image(base_url)
         log.debug(f"Found image for release '{release}': {filename} ({serial})")
         sha256 = self.get_sha256(base_url, filename)
@@ -602,7 +606,11 @@ class RockyHandler(DistroHandler):
             arch = "x86_64"
         if arch == "arm64":
             arch = "aarch64"
-        base_url = self.URL + f"/pub/rocky/{release}/images/{arch}"
+        (major, minor) = (int(i) for i in release.split('.'))
+        if (major == 10 and minor < 2) or (major == 9 and minor < 8) or (major == 8 and minor < 10):
+            base_url = self.URL + f"/vault/rocky/{release}/images/{arch}"
+        else:
+            base_url = self.URL + f"/pub/rocky/{release}/images/{arch}"
         filename, serial = self.get_latest_release_image(base_url)
         log.debug(f"Found image for release '{release}': {filename} ({serial})")
         sha256 = self.get_sha256(base_url, filename)
